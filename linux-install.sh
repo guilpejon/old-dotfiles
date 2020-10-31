ec69957ab8ae95f3c840cbf279265991fc01c602
Guilherme Pejon <guilpejon@gmail.com>
2020-10-31 13:19:33 -0300

    Added linux install

---
+++ linux-install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+if
+  [[ "${USER:-}" == "root" ]]
+then
+  echo "This script works only with normal user, it wont work with root, please log in as normal user and try again." >&2
+  exit 1
+fi
+
+# check if tput exists
+if ! command -v tput &> /dev/null; then
+  # tput could not be found
+  BOLD=""
+  RESET=""
+else
+  BOLD=$(tput bold)
+  RESET=$(tput sgr0)
+fi
+
+install_msg() {
+  printf '%s\n' ----------------------------------------
+  echo "> Installing or Upgrading $1"
+  printf '%s\n' ----------------------------------------
+}
+
+install_or_upgrade() {
+  install_msg $1
+  sudo apt install "$1" "$2" -y
+}
+
+sudo apt-get update -y
+
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+install_or_upgrade nodejs
+
+install_or_upgrade redis-server
+sudo systemctl enable redis-server.service
+
+install_or_upgrade vim-gtk
+install_or_upgrade imagemagick --fix-missing
+install_or_upgrade build-essential
+install_or_upgrade git-core
+install_or_upgrade curl
+install_or_upgrade openssl
+install_or_upgrade libssl-dev
+install_or_upgrade libcurl4-openssl-dev
+install_or_upgrade zlib1g
+install_or_upgrade zlib1g-dev
+install_or_upgrade libreadline6-dev
+install_or_upgrade libyaml-dev
+install_or_upgrade libsqlite3-dev
+install_or_upgrade libsqlite3-0
+install_or_upgrade sqlite3
+install_or_upgrade libxml2-dev
+install_or_upgrade libxslt1-dev
+install_or_upgrade libffi-dev
+install_or_upgrade software-properties-common
+install_or_upgrade libgdm-dev
+install_or_upgrade libncurses5-dev
+install_or_upgrade automake
+install_or_upgrade autoconf
+install_or_upgrade libtool
+install_or_upgrade bison
+install_or_upgrade postgresql
+install_or_upgrade postgresql-contrib
+install_or_upgrade libpq-dev
+install_or_upgrade libc6-dev
+install_or_upgrade snapd
+install_or_upgrade gh
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+install_or_upgrade yarn
+
+install_msg "herou"
+sudo snap install --classic heroku
+
+sudo apt autoremove
+
