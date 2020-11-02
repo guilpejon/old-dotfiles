2823dbe6a086d53a6f65401f2ff6eb6ed5e1278e
Guilherme Pejon <guilpejon@gmail.com>
2020-11-02 09:49:43 -0300

    Fixed vim installation

--- agnostic_install.sh
+++ agnostic_install.sh
@@ -1,8 +1,9 @@
 ############### DEV TOOLS ###############
 
 # install RVM
-curl -kL get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash
 source ~/.rvm/scripts/rvm
+rvm install ruby --latest
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,10 +12,13 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
 # install spaceship theme for ohmyzsh
+sudo chown -R $USER /usr/local
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 awk '!/ZSH_THEME/' $HOME > ~/.temp && mv ~/.temp $HOME # remove current ZSH_THEME
 echo ZSH_THEME="'spaceship'" | sudo tee -a "$HOME/.zshrc" # add spaceship theme
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ############### RAILS ###############
 
